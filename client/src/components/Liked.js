70ce5e085570a7db5ed5cd49648cd30378c4ca9d
Keagan Barnes <barnes.keagan9@gmail.com>
2020-01-04 16:56:05 -0700

    subtle changes

--- client/src/components/Liked.js
+++ client/src/components/Liked.js
@@ -1,76 +1,77 @@
 import React from 'react';
-import { Header, Grid , Image, } from 'semantic-ui-react'
+import { Header, Grid, Image, } from 'semantic-ui-react'
 
 const Liked = () => (
-<>
-<Header>
-  <h1 align="left">Liked Videos</h1>
-</Header>
-<Grid align="left" columns={4}>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>      <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>      <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-        <Grid.Column>
-          <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
-          <h3>GINYU FORCE</h3>
-        </Grid.Column>
-</Grid>
-</>
+  <>
+    <Header>
+      <br />
+      <Header as="h3" textAlign="left">Liked Videos</Header>
+    </Header>
+    <Grid align="left" columns={4}>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+      <Grid.Column>
+        <Image style={four} src='https://www.globalresearch.ca/wp-content/uploads/2017/07/Trump-Dictator-1024x538.png' />
+        <h3>GINYU FORCE</h3>
+      </Grid.Column>
+    </Grid>
+  </>
 );
 
 
